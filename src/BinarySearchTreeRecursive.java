b11499574fae453552943be2675cfd79dd5a7c7a
GIL32610 <gil.00000032610@unicap.br>
2023-03-17 16:16:57 -0300

    Meotodo de remoção para subárvores não completas

--- src/BinarySearchTreeRecursive.java
+++ src/BinarySearchTreeRecursive.java
@@ -1,29 +1,29 @@
-public class BinarySearchTreeRecursive{
+public class BinarySearchTreeRecursive {
     private Node root;
 
-    public void insertNode(int number){
+    public void insertNode(int number) {
         root = insertNode(number, root);
     }
 
-    Node insertNode(int number, Node node){
-        if(node == null){
+    Node insertNode(int number, Node node) {
+        if (node == null) {
             return new Node(number);
-        }else if(number < node.getData()){
+        } else if (number < node.getData()) {
             node.setLeft(insertNode(number, node.getLeft()));
-        }else if(number>node.getData()){
+        } else if (number > node.getData()) {
             node.setRight(insertNode(number, node.getRight()));
-        }else{
+        } else {
             System.out.println("Repeated");
         }
         return node;
     }
 
-    public void inOrder(){
+    public void inOrder() {
         inOrder(root);
     }
 
-    private void inOrder(Node root){
-        if(root == null){
+    private void inOrder(Node root) {
+        if (root == null) {
             return;
         }
         inOrder(root.getLeft());
@@ -31,13 +31,30 @@ public class BinarySearchTreeRecursive{
         inOrder(root.getRight());
     }
 
-    public void deleteNode(int number){
-        root = deleteNode(root, number);
+    public void deleteNode(int number) {
+        if (root != null) {
+            root = deleteNode(root, number);
+        } else
+            System.out.println("Empty!");
     }
 
-    Node deleteNode(Node node, int number){
-        
-
-
+    Node deleteNode(Node node, int number) {
+        if (node != null) {
+            if (node.getData() == number) {
+                if (node.getLeft() == null && node.getRight() == null)
+                    node = null;
+                else if (node.getLeft() == null)
+                    node = node.getRight();
+                else if (node.getRight() == null)
+                    node = node.getLeft();
+                else{
+                    
+                }
+            }
+        } else if (node.getData() < number) {
+            node.setLeft(deleteNode(node.getLeft(), number));
+        } else
+            node.setRight(deleteNode(node.getRight(), number));
+        return node;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        
+        BinarySearchTreeRecursive tree = new BinarySearchTreeRecursive();
+        tree.insertNode(8);
+        tree.insertNode(12);
+        tree.insertNode(4);
+        tree.insertNode(10);
+        tree.insertNode(9);
+        tree.insertNode(11);
+        tree.removeNode(10);
     }
 
     public static int max(int num1, int num2) {
